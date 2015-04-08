ecedd3ef4be6fe4e5bc283718bf03ff78296765f
Informatica <informatica@scoutsengidsenvlaanderen.be>
2015-04-08 18:31:59 +0200

    Removed confusing file description

--- plugins/access.fs/class.fsAccessDriver.php
+++ plugins/access.fs/class.fsAccessDriver.php
@@ -1278,7 +1278,7 @@ class fsAccessDriver extends AbstractAccessDriver implements AjxpWrapperProvider
         $metaData["file_perms"] = $fPerms;
         $datemodif = $this->date_modif($ajxpNode->getUrl());
         $metaData["ajxp_modiftime"] = ($datemodif ? $datemodif : "0");
-        $metaData["ajxp_description"] =$metaData["ajxp_relativetime"] = $mess[4]." ".AJXP_Utils::relativeDate($datemodif, $mess);
+        $metaData["ajxp_description"] = '';
         $metaData["bytesize"] = 0;
         if ($isLeaf) {
             $metaData["bytesize"] = filesize($ajxpNode->getUrl());
