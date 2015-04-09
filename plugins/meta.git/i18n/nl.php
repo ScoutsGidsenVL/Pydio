b98847f1ed6ea85b1d08e35a02a0f1df67f322bc
Informatica <informatica@scoutsengidsenvlaanderen.be>
2015-04-09 10:41:28 +0200

    Translation added for meta.git

---
+++ plugins/meta.git/i18n/nl.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+
+defined('AJXP_EXEC') or die('Access not allowed');
+
+$mess = array(
+    "1" => "Versies",
+    "2" => "Bekijk vorige versies",
+    "3" => "Download",
+    "4" => "Download de geselecteerde versie",
+    "5" => "Bekijk (online)",
+    "6" => "Open de geselecteerde versie in een apart tabblad",
+    "7" => "Draai terug...",
+    "8" => "Draai het bestand terug naar de selecteerde versie",
+    "9" => "#",
+    "10" => "Datum",
+    "11" => "Opgeladen door",
+    "12" => "Aanpassing",
+    "13" => "Ben je zeker dat je het bestand wilt terugdraaien naar de selecteerde versie?\n\nEr worden geen versies verwijderd. De selecteerde versie wordt gekopieerd naar een nieuwe versie.",
+    "RENAME"        => "Bestand hernoemd",
+    "MOVE"          => "Bestand verplaatst",
+    "MODIFICATION"  => "Inhoud aangepast",
+    "DELETION"      => "Bestand verwijderd",
+    "CREATION"      => "Bestand aangemaakt"
+);
