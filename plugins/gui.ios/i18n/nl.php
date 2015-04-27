e6a45c717bc75a4ca384fdd3ab8e40dbf3dff945
Informatica <informatica@scoutsengidsenvlaanderen.be>
2015-04-27 13:42:12 +0200

    Added Dutch translations

---
+++ plugins/gui.ios/i18n/nl.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"1.ios" => "Wil je de iPhone/iPad-app voor %s gebruiken?",
+"2.ios" => "Ja, ik heb die app al. Gebruik de app om naar %s te gaan.",
+"3.ios" => "Ja, ik heb die app nog niet. Waar vind ik die app in de App Store?",
+"4.ios" => "Nee, ik gebruik liever de web versie van %s.",
+"1.android" => "Wil je de android-app voor %s gebruiken?",
+"2.android" => "Ja, ik heb die app al. Gebruik de app om naar %s te gaan.",
+"3.android" => "Ja, ik heb die app nog niet. Waar vind ik die app in de Play Store?",
+"4.android" => "Nee, ik gebruik liever de web versie van %s.",
+);
