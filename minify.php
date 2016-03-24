b0cdf06d401c33abb9ce34b3e416f5c0ad755024
Informatica <informatica@scoutsengidsenvlaanderen.be>
2016-03-24 18:27:03 +0100

    Improved minify.php

--- minify.php
+++ minify.php
@@ -1,31 +1,30 @@
 <?php
-	 // Get the file path and get the extension from it.
-    $fileParts = pathinfo($_GET['css']);
 
-   	if (! empty($_GET['css'])) {
-      if ($fileParts['extension'] == 'css') {
-        $buffer = '';
-        $buffer .= file_get_contents($_GET['css']);
+    header("Content-type: text/css");
+    // Other headers should be handled with Nginx / Apache.
 
-        $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
-        $buffer = str_replace(': ', ':', $buffer);
-        $buffer = str_replace(array("\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
+    if (! empty($_GET['css'])) {
 
-        // We do not do this. Because it should be handled with Nginx / Apache.
-        // ob_start("ob_gzhandler");
+        // No URLS and no paths like '/usr/local/www/pydio/plugins/../../../...'
+        $path = realpath('./' . $_GET['css']);
 
-        header('Cache-Control: public');
-        header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT');
-        header("Content-type: text/css");
+        if ($path !== false
+                && preg_match('#^/usr/local/www/pydio/plugins/[a-z0-9_/\.]*\.css$#', $path) === 1) {
 
-        // var_dump($buffer);
-        echo $buffer;
-      } else {
-        header("Content-type: text/css");
-        echo "/* Er moet een css bestand worden aangegeven. */";
-      }
-    } else {
-      header('Content-Type: text/css'); 
-      echo "/* U moet een bestand ingeven aan minify.php. bv: minify.php?css=style.css */";
+            $buffer = file_get_contents($path);
+
+            if (! empty($buffer)) {
+
+                $buffer = preg_replace('#/\*[^*]*\*+([^/][^*]*\*+)*/#', '', $buffer);
+                $buffer = str_replace(': ', ':', $buffer);
+                $buffer = str_replace(array("\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
+
+                echo $buffer;
+
+                die();
+            }
+        }
     }
+
+    echo "/* Please specify a css file. For example: minify.php?css=style.css */";
 ?>
